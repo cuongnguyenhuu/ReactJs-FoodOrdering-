00f3f8f44ae7f7fc4c00473a7c3a073ea9493529
Cuong (currie) H.NGUYEN <currie@enclave.vn>
2019-07-11 14:49:32 +0700

    test commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import './App.css';
 
 function App() {
   return (
-    <div class="dropdown">
-  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-    Dropdown button
-  </button>
-  <div class="dropdown-menu">
-    <a class="dropdown-item" href="#">Link 1</a>
-    <a class="dropdown-item" href="#">Link 2</a>
-    <a class="dropdown-item" href="#">Link 3</a>
-  </div>
-</div>
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      <strong></strong> 
+    </div>
+    
+    <script>
+      $(".alert").alert();
+    </script>
   );
 }
 
