dac41ae49f55e0394c0840409a08fdc1cdb527b9
Cuong (Currie) H.NGUYEN <cuongnguyenhuu97@gmail.com>
2019-07-12 22:51:13 +0700

    test in local computer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      <strong></strong> 
+    <div>
+      <div class="btn-group">
+        <button type="button" class="btn btn-secondary">Split dropdown</button>
+        <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <span class="sr-only">Toggle Dropdown</span>
+        </button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item disabled" href="#">Disabled action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Separated link</a>
+        </div>
+      </div>
     </div>
-    
-    <script>
-      $(".alert").alert();
-    </script>
   );
 }
 
